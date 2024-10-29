4d1defe0f8dad71f44174f3216c4494860d31ad3
Asahi <163096912+asahitan@users.noreply.github.com>
2024-10-29 17:24:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+function showSpecialMessage() {
+    document.getElementById("specialMessage").textContent = "You make my heart complete, and I can’t wait to see what the future holds for us. Happy 4th monthsary, my love!";
+}
 
